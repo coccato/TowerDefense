dc5019b2c83c8a42dd6ce9163625bf27ea90284d
coccato <riccardo.coccato@itiszuccante.edu.it>
2026-04-21 14:14:05 +0200

    Branch

--- src/main/java/com/example/towerdefense/Main.java
+++ src/main/java/com/example/towerdefense/Main.java
@@ -1,4 +1,12 @@
 package com.example.towerdefense;
 
-public class Main {
+public  class Main {
+    int a = 0;
+
+
+    public boolean prova() {
+        int a = 0;
+        if (a == 0) return true;
+        return false;
+    }
 }
